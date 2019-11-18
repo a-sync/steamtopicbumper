d6af313ad652b8da830f8aa5104ce593696de508
smith <andras@sexybuddha.hu>
2019-11-18 21:28:26 +0100

    remember login wip

--- puppeteer.js
+++ puppeteer.js
@@ -1,28 +1,35 @@
 const puppeteer = require('puppeteer');
 const fetch = require('node-fetch');
+const {waitForNewMail, clearInbox} = require('./restmail.js');
 
-const LOGIN_URL = process.env.LOGIN_URL + '%3Ftscn%3D18446744073709551615';
+const LOGIN_URL = process.env.LOGIN_URL;
 const LOGIN = process.env.LOGIN;
 const PASSW = process.env.PASSW;
 const RESTMAIL = process.env.RESTMAIL; // @restmail.net (forward steam auth code emails here)
 const TOKEN = process.env.TOKEN;
 
+function getBrowser() {
+    if (TOKEN) {
+        return puppeteer.connect({
+            browserWSEndpoint: 'wss://chrome.browserless.io/?token='+TOKEN
+            +'&--disable-dev-shm-usage=true&--user-data-dir=~/bumper-'+LOGIN,
+            ignoreHTTPSErrors: true
+        });
+    } else {
+        return puppeteer.launch({
+            args:[
+                '--disable-dev-shm-usage'
+            ],
+            userDataDir: __dirname + '/userData-'+LOGIN,
+            ignoreHTTPSErrors: true,
+            headless: true
+        });
+    }
+}
+
 async function bump() {
     console.time('connection');
-    const browser = await puppeteer.connect({
-        browserWSEndpoint: 'wss://chrome.browserless.io/?token='+TOKEN
-        +'&--disable-dev-shm-usage=true&--user-data-dir=~/bumper-'+LOGIN,
-        ignoreHTTPSErrors: true
-    });
-    /*const browser = await puppeteer.launch({
-        args:[
-            //'--window-size=600,800',
-            '--disable-dev-shm-usage'
-        ],
-        userDataDir: __dirname + '/userData-'+LOGIN,
-        ignoreHTTPSErrors: true,
-        headless: true
-    });*/
+    const browser = await getBrowser();
 
     let closing = false;
     let closePromise;
@@ -45,51 +52,53 @@ async function bump() {
             }
         });
 
-        console.time('login');
         const page = await browser.newPage();
         page.setDefaultTimeout(10000);
         await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36');
-        //await page.setViewport({width: 600, height: 800});
+        await page.goto(LOGIN_URL + '%3Ftscn%3D18446744073709551615', {waitUntil: 'load'});
 
         // Log in
-        try {
-            await page.goto(LOGIN_URL, {waitUntil: 'load'});
-            await page.type('#steamAccountName', LOGIN);
-            await page.type('#steamPassword', PASSW);
-            await Promise.race([
-                Promise.all([
-                    page.waitForNavigation({waitUntil: 'load'}).catch(()=>{}),
-                    page.click('#SteamLogin')
-                ]),
-                page.waitForSelector('#authcode', {visible:true}).catch(()=>{})
-            ]);
-        } catch (error) {
-            console.error(error.message);
-            console.timeEnd('login');
-            return shutDown('Login failed!');
-        }
-        console.timeEnd('login');
-
-        // Deal with Steam Guard
-        if (await page.$('#authcode')) {
-            console.time('authcode');
+        if (await page.$('#steamAccountName')) {
+            console.time('login');
             try {
-                const mailBody = await waitForNewMail();
-                clearInbox();
-                const authCode = mailBody.split(LOGIN+':\n\n')[1].split('\n')[0];
-                await page.type('#authcode', authCode);
-                await page.click('#auth_buttonset_entercode div[data-modalstate="submit"]');
-                await page.waitForSelector('#auth_buttonset_success a[data-modalstate="complete"]', {visible:true});
-                await Promise.all([
-                    page.waitForNavigation({waitUntil: 'load'}),
-                    page.click('#auth_buttonset_success a[data-modalstate="complete"]')
+                await page.type('#steamAccountName', LOGIN);
+                await page.type('#steamPassword', PASSW);
+                page.click('#remember_login').catch(()=>{});
+                await Promise.race([
+                    Promise.all([
+                        page.waitForNavigation({waitUntil: 'load'}).catch(()=>{}),
+                        page.click('#SteamLogin')
+                    ]),
+                    page.waitForSelector('#authcode', {visible:true}).catch(()=>{})
                 ]);
-            } catch(error) {
+            } catch (error) {
                 console.error(error.message);
-                console.timeEnd('authcode');
-                return shutDown('Login with authcode failed!');
+                console.timeEnd('login');
+                return shutDown('Login failed!');
             }
-            console.timeEnd('authcode');
+            console.timeEnd('login');
+            
+            // Deal with Steam Guard
+            if (await page.$('#authcode')) {
+                console.time('authcode');
+                try {
+                    const mailBody = await waitForNewMail(RESTMAIL);
+                    clearInbox(RESTMAIL);
+                    const authCode = mailBody.split(LOGIN+':\n\n')[1].split('\n')[0];
+                    await page.type('#authcode', authCode);
+                    await page.click('#auth_buttonset_entercode div[data-modalstate="submit"]');
+                    await page.waitForSelector('#auth_buttonset_success a[data-modalstate="complete"]', {visible:true});
+                    await Promise.all([
+                        page.waitForNavigation({waitUntil: 'load'}),
+                        page.click('#auth_buttonset_success a[data-modalstate="complete"]')
+                    ]);
+                } catch(error) {
+                    console.error(error.message);
+                    console.timeEnd('authcode');
+                    return shutDown('Login with authcode failed!');
+                }
+                console.timeEnd('authcode');
+            }
         }
 
         // Add new reply
@@ -139,50 +148,6 @@ async function bump() {
     }
 
     return shutDown();
-}
-
-async function loadLastFromInbox() {
-    //console.debug(`Checking ${RESTMAIL}@restmail.net @ ` + String(new Date()));
-    const res = await fetch('http://restmail.net/mail/'+RESTMAIL);
-    const resJson = await res.json();
-
-    try {
-        const {text, date} = resJson.pop();
-        const currDate = new Date();
-        const mailDate = new Date(date);
-        const age = Math.abs(currDate.getTime() - mailDate.getTime());
-        return {text, age};
-    } catch (error) {
-        return new Error('No messages.');
-    }
-}
-
-function waitForNewMail() {
-    return new Promise((resolve, reject) => {
-        let i = 0;
-
-        const check = () => {
-            loadLastFromInbox().then(res => {
-                if (!(res instanceof Error) && res.age / 1000 < 30) {
-                    console.log('Found new mail on try nr. ' + String(i+1));
-                    return resolve(res.text);
-                } else {
-                    i++;
-                    if (i < 10) {
-                        setTimeout(check, 2500);
-                    } else {
-                        return reject(new Error('No new message received within the time limit.'));
-                    }
-                }
-            });
-        };
-
-        setTimeout(check, 5000);
-    });
-}
-
-function clearInbox() {
-    return fetch('http://restmail.net/mail/'+RESTMAIL, {method:'DELETE'});
 }
 
 module.exports.bump = bump;
