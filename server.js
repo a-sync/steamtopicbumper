b8ae68568646fd4b6658283a54793a9530c7b543
smith <andras@sexybuddha.hu>
2019-12-04 23:37:35 +0100

    capture output and serve it via http

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+const httpOutArray = [];
+for (const f of ['log','warn','error','debug']) {
+    console['_'+f] = console[f];
+    console[f] = (...args) => {
+        while (httpOutArray.length >= 200) httpOutArray.shift();
+        httpOutArray.push(args.map(i => String(i)).join(' '));
+        return console['_'+f](...args);
+    };
+}
+
 const {bump} = require('./puppeteer.js');
 //require('dotenv').config();
 
 console.log('Starting bumper...');
-let counter = 0;
+let counter = 1500;
 
 async function run() {
     const c = ++counter;
@@ -17,3 +27,9 @@ async function run() {
 
 setInterval(run, 1000 * 60 * 60);
 run();
+
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Nr. of bumps: '+counter+'\n\n'+httpOutArray.join('\n'));
+}).listen(80, '0.0.0.0');
