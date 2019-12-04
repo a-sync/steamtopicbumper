6501fca0964ec361b4bbeb4b02fa45c89d2d8f7f
smith <andras@sexybuddha.hu>
2019-12-05 00:49:10 +0100

    misc

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ for (const f of ['log', 'warn', 'error']) {
 }
 
 // Serve captured data
-var http = require('http');
+const http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Events: '+JSON.stringify(events,null,2).replace(/"/g,'')
@@ -40,7 +40,6 @@ http.createServer(function (req, res) {
 // Run bot
 //require('dotenv').config();
 const {bump} = require('./puppeteer.js');
-
 async function run() {
     const c = ++events.bumps;
     console.log('Starting bump #' + c + ' @ ' + String(new Date()));
